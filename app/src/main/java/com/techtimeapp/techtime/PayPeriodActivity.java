55ce474333b28b4fb7ca04c622430dd20b4b74e8
KennethBowen <kencbowen@gmail.com>
2018-08-11 11:57:09 -0700

    secondary commit

---
+++ app/src/main/java/com/techtimeapp/techtime/PayPeriodActivity.java
@@ -0,0 +1,13 @@
+package com.techtimeapp.techtime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PayPeriodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_period);
+    }
+}
