e9279f04903d997a1a3ba332a34b788bd6b5542d
KennethBowen <kencbowen@gmail.com>
2018-09-01 11:42:42 -0700

    created Room Persistence Database

--- app/src/main/java/com/techtimeapp/techtime/view/view/data/PayPeriod.java
+++ app/src/main/java/com/techtimeapp/techtime/view/view/data/PayPeriod.java
@@ -1,12 +1,24 @@
 package com.techtimeapp.techtime.view.view.data;
 
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+
 import java.util.ArrayList;
 import java.util.Date;
 
+@Entity
 public class PayPeriod {
+    @ColumnInfo(name = "repair_order_list")
     private ArrayList<RepairOrder> mRepairOrders;
+
+    @ColumnInfo(name = "Start_Date")
     private Date mStartDate;
+
+    @ColumnInfo(name = "End_Date")
     private Date mEndDate;
+
+    @ColumnInfo(name = "Number_of_Repair_Orders")
     private int mNumberOfRepairOrders;
 
     public PayPeriod(ArrayList<RepairOrder> repairOrders, Date startDate, Date endDate, int numberOfRepairOrders) {
--- app/src/main/java/com/techtimeapp/techtime/view/view/data/RepairOrder.java
+++ app/src/main/java/com/techtimeapp/techtime/view/view/data/RepairOrder.java
@@ -1,25 +1,50 @@
 package com.techtimeapp.techtime.view.view.data;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.Date;
 
+@Entity
 public class RepairOrder {
+    @PrimaryKey
+    private int mUid;
+
+    @ColumnInfo(name = "Repair_Order_number")
     private int mRepairOrderNumber;
+
+    @ColumnInfo(name = "Writer")
     private String mWriter;
+
+    @ColumnInfo(name = "Customer")
     private String mCustomer;
+
+    @ColumnInfo(name = "Date")
     private Date mDate;
+
+    @ColumnInfo(name = "Labor_Rate")
     private LaborRate mLaborRate;
+
+    @ColumnInfo(name = "Vehicle")
     private Vehicle mVehicle;
+
+    @ColumnInfo(name = "Hours")
     private double mHours;
+
+    @ColumnInfo(name = "Gross")
     private double mGross;
 
-    public RepairOrder(int repairOrderNumber, String writer, Date date, LaborRate laborRate) {
+    public RepairOrder(int Uid, int repairOrderNumber, String writer, Date date, LaborRate laborRate) {
+        mUid = Uid;
         mRepairOrderNumber = repairOrderNumber;
         mWriter = writer;
         mDate = date;
         mLaborRate = laborRate;
     }
 
-    public RepairOrder(int repairOrderNumber, String writer, Date date,  String customer, LaborRate laborRate, Vehicle vehicle, double hours, double gross) {
+    public RepairOrder(int Uid, int repairOrderNumber, String writer, Date date,  String customer, LaborRate laborRate, Vehicle vehicle, double hours, double gross) {
+        mUid = Uid;
         mRepairOrderNumber = repairOrderNumber;
         mWriter = writer;
         mDate = date;
