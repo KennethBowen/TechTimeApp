274a580eb7df00e07e06af2b99a7a44f346ef2db
KennethBowen <kencbowen@gmail.com>
2018-11-10 12:37:55 -0800

    refactored gradle

--- app/src/main/java/com/techtimeapp/techtime/view/view/data/RepairOrder.java
+++ app/src/main/java/com/techtimeapp/techtime/view/view/data/RepairOrder.java
@@ -1,10 +1,5 @@
 package com.techtimeapp.techtime.view.view.data;
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
-
-import java.time.Year;
 import java.util.Date;
 
 
